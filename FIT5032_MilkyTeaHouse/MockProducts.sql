573081ee7568ae5a612ba800d5491874ef7ad35b
Queenie-7 <522356165@qq.com>
2020-08-24 21:24:04 +0800

    update

---
+++ FIT5032_MilkyTeaHouse/MockProducts.sql
@@ -0,0 +1,25 @@
+﻿SET IDENTITY_INSERT [dbo].[AspNetProducts] ON
+INSERT INTO [dbo].[AspNetProducts] ([ProductId], [ProductName], [ProductPrice], [ProductIntro]) VALUES (1, N'Rose&Green tea-milk', 20, N'yummy')
+INSERT INTO [dbo].[AspNetProducts] ([ProductId], [ProductName], [ProductPrice], [ProductIntro]) VALUES (2, N'Ginseng&Oolong tea-milk', 20, N'yummy')
+INSERT INTO [dbo].[AspNetProducts] ([ProductId], [ProductName], [ProductPrice], [ProductIntro]) VALUES (3, N'Peach &oolong tea-milk', 20, N'yummy')
+INSERT INTO [dbo].[AspNetProducts] ([ProductId], [ProductName], [ProductPrice], [ProductIntro]) VALUES (4, N'Green tea-rayal ', 20, N'yummy')
+INSERT INTO [dbo].[AspNetProducts] ([ProductId], [ProductName], [ProductPrice], [ProductIntro]) VALUES (5, N'Orange& Oolong tea-milk', 20, N'yummy')
+INSERT INTO [dbo].[AspNetProducts] ([ProductId], [ProductName], [ProductPrice], [ProductIntro]) VALUES (6, N'Papaya Smoothies', 16, N'yummy')
+INSERT INTO [dbo].[AspNetProducts] ([ProductId], [ProductName], [ProductPrice], [ProductIntro]) VALUES (7, N'Pineapple Smoothies
+ ', 16, N'yummy')
+INSERT INTO [dbo].[AspNetProducts] ([ProductId], [ProductName], [ProductPrice], [ProductIntro]) VALUES (8, N'Kiwi fruit Smoothies
+ ', 16, N'yummy')
+INSERT INTO [dbo].[AspNetProducts] ([ProductId], [ProductName], [ProductPrice], [ProductIntro]) VALUES (9, N'Peach Smoothies
+', 16, N'yummy')
+INSERT INTO [dbo].[AspNetProducts] ([ProductId], [ProductName], [ProductPrice], [ProductIntro]) VALUES (10, N'Cantaloupe Smoothies', 16, N'yummy')
+INSERT INTO [dbo].[AspNetProducts] ([ProductId], [ProductName], [ProductPrice], [ProductIntro]) VALUES (11, N'Herbal jelly milk tea-milk', 18, N'yummy')
+INSERT INTO [dbo].[AspNetProducts] ([ProductId], [ProductName], [ProductPrice], [ProductIntro]) VALUES (12, N'Pearl milk tea,rose taste', 18, N'yummy')
+INSERT INTO [dbo].[AspNetProducts] ([ProductId], [ProductName], [ProductPrice], [ProductIntro]) VALUES (13, N'Pearl milk tea,lychee taste ', 18, N'yummy')
+INSERT INTO [dbo].[AspNetProducts] ([ProductId], [ProductName], [ProductPrice], [ProductIntro]) VALUES (14, N'Pearl milk tea，peach taste
+', 18, N'yummy')
+INSERT INTO [dbo].[AspNetProducts] ([ProductId], [ProductName], [ProductPrice], [ProductIntro]) VALUES (15, N'Red beans 
+milk tea
+ ', 18, N'yummy')
+INSERT INTO [dbo].[AspNetProducts] ([ProductId], [ProductName], [ProductPrice], [ProductIntro]) VALUES (16, N'Herbal jelly &Pearl milk tea', 18, N'yummy')
+INSERT INTO [dbo].[AspNetProducts] ([ProductId], [ProductName], [ProductPrice], [ProductIntro]) VALUES (17, N'Oreo cheese balck tea', 18, N'yummy')
+SET IDENTITY_INSERT [dbo].[AspNetProducts] OFF
---
+++ FIT5032_MilkyTeaHouse/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032_MilkyTeaHouse.Startup))]
+namespace FIT5032_MilkyTeaHouse
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            app.MapSignalR();
+        }
+    }
+}
